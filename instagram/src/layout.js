2cdb2f79eaa56738d87f8d37c001679b4625e745
soniasych <sychsofia@gmail.com>
2019-05-08 15:14:06 +0300

    trying to adding comments

--- instagram/src/layout.js
+++ instagram/src/layout.js
@@ -41,10 +41,106 @@ export class Layout extends Component {
                     commentAuthor: "olivi_k",
                     commentText: "Is it real picture?"
                 }
-            ]
+            ],
+            defaultComment:  {
+                commentId: 6,
+                commentAuthor: "rgosling",
+                commentText: "La La Land"
+            },
+            showComments: false,
+            newComment:  {
+                commentId: 7,
+                commentAuthor: "nasa",
+                commentText: ""
+            },
+            VeryNewComment: {}
         };
-    }    
 
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        //this.updateInputValue = this.updateInputValue.bind(this);
+    }
+
+   
+
+    /* handleChange(event){
+        this.setState(
+            {defaultComment: event.target.defaultComment}
+            );
+    } */
+
+    /* handleChange(event){
+        this.setState(
+            {comments: this.state.comments.concat({defaultComment: ""})}
+            );
+    } */
+    
+    /* updateInputValue(evt){
+        //console.log("input field updated with "+evt.target.value);
+        this.state={newComment: evt.target.value};   
+    
+      } */
+
+   /*  handleChange(event){
+        var newArray = this.state.comments.slice();   
+        newArray.push(this.state.newComment);   
+        this.setState({comments:newArray})
+    } */
+
+    handleChange = (e) => {
+        this.setState({
+            newComment: e.target.value
+        })
+    }
+
+    /* handleSubmit(event){
+        let submit = this.state.newComment;
+            //event.PreventDefault();
+            return(submit);
+        
+    } */
+
+    addValue(evt)
+    {
+      evt.preventDefault();
+      if(this.state.newComment.commentText !=undefined)
+      {
+        alert('Your input value is: ' + this.state.newComment.commentText)
+      }
+    }
+
+    updateInput(evt){
+        this.state={value: evt.target.value};   
+          }
+
+
+    handleSubmit(event){
+        event.preventDefault();
+        var newArray = this.state.comments.slice();   
+        const form = {
+            commentId: this.state.newComment.commentId,
+            commentAuthor: this.state.newComment.commentAuthor,
+            commentText: this.state.newComment.commentText
+           }
+
+           newArray.push(form);
+           this.setState({
+                comments: {
+                    commentId: '', 
+                    commentAuthor: '',
+                    commentText: ''
+            }
+           })
+        
+    }
+
+
+    ShowNewCommentHandler = () => {
+        const doesShow = this.state.showComments;
+        this.setState({showComments: !doesShow});
+    }
+
+    
     renderCommentList = () => {       
         return(<ul>
             { this.state.comments.map(
@@ -59,6 +155,27 @@ export class Layout extends Component {
 
     render() {
        let сomments = this.renderCommentList();
+       let outputComments = null;
+       
+       if(this.state.showComments)
+       {
+           outputComments = (
+            <div>
+                {сomments}
+            </div>
+           );
+       }
+
+       /* let commentCopy = JSON.parse(JSON.stringify(this.state.comments))
+       //make changes to ingredients
+       commentCopy[2].commentText = //whatever new ingredients are
+       this.setState({
+          comments:commentCopy 
+        }); */
+
+
+
+       //let newComment = this.state.defaultComment.commentText;
         return (
             <div className="currentLayout">
                 <div className="userOptions">
@@ -96,7 +213,28 @@ export class Layout extends Component {
                 </div>
                 <div>Comments
                     <div>
+                        <a href="#" onClick={this.ShowNewCommentHandler} >Show comments</a>
+                    </div>
+                    {/* { this.state.showComments ?
+                    <div>
                     {сomments}
+                    </div> : null
+                    } */}
+                    {outputComments}
+                    <div>Add comment
+                        <form onSubmit={this.addValue} >
+                        {/* <form onSubmit={this.handleSubmit} > */}
+                        {/* <form> */}
+                            <label>Nasa
+                           {/*  <input type="text" id="inputfield" value={this.state.newComment.commentText} onChange={e => this.handleChange(e)} /> */}
+                                 <input type="text" id="inputfield" onChange={(e) => this.updateInput(e)}>
+                                  
+                                </input> 
+                            </label>
+                            {/* <input onClick={() => this.handleChange()} type="submit" value="add"/> */}
+                            <input  type="submit" value="add"/>
+                        </form>
+                        
                     </div>
                 </div>
             </div>

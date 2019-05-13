b49e3fcdb983de465100ed65cb0dae0f85df8b7e
soniasych <sychsofia@gmail.com>
2019-05-13 15:45:51 +0300

    adding GET request for posts

--- instagram/src/Layout/PostLayout/PostLayout.js
+++ instagram/src/Layout/PostLayout/PostLayout.js
@@ -11,60 +11,85 @@ import SaveIcon from '../../assets/instagram-save.png';
 
 import './PostLayout.css';
 import { Link } from 'react-router-dom';
+import Axios from 'axios';
 
 export class PostLayout extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            showComments: false
+            post: []
         };
     }
 
-    render() {
-        return (
-            <div className="currentLayout">
-                <div className="userOptions">
-                    <div className="userNickImage">
-                        <img src={NasaLogo} alt="NasaLogo" />
-                    </div>
-                    <div>
-                        nasa
-                    </div>
-                    <div className="userImageOptions">
-                        <img src={InstagramOptions} alt="InstagramOptions" />
-                    </div>
-                </div>
-                <div className="userImage">
-                    <img src={SaturnImage} alt="Saturn" />
-                </div>
-                <div className="userOptions">
-                    <div>
-                        <img src={LikeIcon} alt="LikeIcon" />
-                    </div>
-                    <div>
-                        <img src={CommentIcon} alt="CommentIcon" />
-                    </div>
-                    <div>
-                        <img src={ShareIcon} alt="ShareIcon" />
-                    </div>
-                    <div>
-                        <img src={SaveIcon} alt="SaveIcon" />
-                    </div>
-                </div>
-                <div className="userImageDescription">
-                    <div>Likes amount</div>
-                    <div>
-                        <label>nasa</label>
-                        <span>Some description</span>
-                    </div>
-                </div>
+    componentDidMount() {
+        this.getPostData();
+    }
 
-                <div>
+    render() {
+        let post = this.state.post;
+        return (
+            <div>
+                { post.map(post => 
+                <div  key={post.id} className="currentLayout">
+                    <div className="userOptions">
+                        <div className="userProfileImage">
+                            <img src={post.avatar} alt="NasaLogo" />
+                        </div>
+                        <div className="userNickname">
+                            <label>
+                                {post.userName} 
+                            </label>  
+                        </div>
+                        <div className="userImageOptions">
+                            <img src={InstagramOptions} alt="InstagramOptions" />
+                        </div>
+                    </div>
+                    <div className="userImage">
+                        <img src={post.imageUrl} alt="Saturn" />
+                    </div>
+                    <div className="userOptions">
+                        <div>
+                            <img src={LikeIcon} alt="LikeIcon" />
+                        </div>
+                        <div>
+                            <img src={CommentIcon} alt="CommentIcon" />
+                        </div>
+                        <div>
+                            <img src={ShareIcon} alt="ShareIcon" />
+                        </div>
+                        <div>
+                            <img src={SaveIcon} alt="SaveIcon" />
+                        </div>
+                    </div>
+                    <div className="userImageDescription">
+                        <div>
+                            <label>{post.likes}</label>
+                            <label>Likes</label>
+                        </div>
+                        <div>
+                            <label>{post.userName}</label>
+                            <span>Some description</span>
+                        </div>
+                    </div>
+                
                     <div>
-                        <Link id="comments" to="/comments">Show all comments</Link>
+                        <div>
+                            <Link id="comments" to="/comments">Show all comments</Link>
+                        </div>
                     </div>
                 </div>
+                )}
             </div>
         );
     }
+
+    async getPostData() {
+        const id = this.props.match.params.id;
+        const response = await Axios.get(`https://5b27755162e42b0014915662.mockapi.io/api/v1/posts/`);
+        const data = await response.data;
+        this.setState({post: data});
+        console.log(data);
+        console.log(this.state.post);
+    }
+
 }

0c72905c738991f4e82ff1a867feb44720825d5b
soniasych <sychsofia@gmail.com>
2019-05-15 14:44:01 +0300

    adding file input on NewPost

--- instagram/src/Layout/NewPost/NewPost.js
+++ instagram/src/Layout/NewPost/NewPost.js
@@ -1,35 +1,51 @@
 import React, { Component } from 'react';
-import Saturn from '../../assets/saturn.jpg';
-import NasaLogo from '../../assets/NASA_logo.png';
 import BackArrow from '../../assets/instagram-arrows-left.png';
 import Axios from 'axios';
+import { Link } from 'react-router-dom';
 import './NewPost.css';
 
 export class NewPost extends Component{
     constructor(props) {
         super(props);
         this.state = {
-            newPost : [
-                {
-                    imageUrl: Saturn,
-                    likes: "0",
-                    userName: "nasa",
-                    avatar: NasaLogo,
-                    description: ""
-                }
-            ],
             description: "",
-            imageUrl: Saturn,
-            likes: "0"
+            imageUrl: undefined,
+            likes: "0", 
+            isLoading: true,
+            selectedFile: undefined,
+            selectedFileName: undefined,
+            value: ''
         };
 
         this.onTextChange = this.onTextChange.bind(this);
-
+        this.handleChange = this.handleChange.bind(this);
+        this.handleImageClick = this.handleImageClick.bind(this);
     }
 
     onTextChange = event => {
         this.setState({description: event.target.value});
     }
+
+    /* handleChange(selectorFiles)
+    {
+        console.log(selectorFiles);
+    } */
+
+    handleChange(event) {
+        if (event.target.files[0]) {
+            this.setState({
+                selectedFile: event.target.files[0],
+                selectedFileName: event.target.files[0].name,
+                imageUrl: window.URL.createObjectURL(event.target.files[0]),
+                value: event.target.value,
+            });
+        }
+    }
+
+    handleImageClick = () => {
+        var e = document.getElementById("user-image");
+        e.style.visibility = 'visable';
+    } 
 
     postDataHandler = () => {
         const data = {
@@ -62,7 +78,9 @@ export class NewPost extends Component{
                 <div className="MainNewPost-header">
                     <div className="NewPost-header">
                         <div>
-                            <img src={BackArrow} alt="BackArrow"/>
+                            <Link to="/">
+                                <img src={BackArrow} alt="BackArrow"/>
+                            </Link>
                         </div>
                         <div>
                             <label>
@@ -76,13 +94,20 @@ export class NewPost extends Component{
                         </div>
                     </div>
                 </div>
+                <div className="NewPost-container">
+                    <div className="NewPost-user-image">
+                        <img src={this.state.imageUrl} id="user-image" alt="ImageLoaded" />
+                    </div>
+                    <div className="NewPost-input-file">
+                        {/* <input type="file" onClick={this.state.isLoading} onChange={ (e) => this.handleChange(e.target.files) }/> */}
+                        <input name="file" id="file" type="file" onClick={this.handleImageClick} onChange={this.handleChange}/>
+                        <label for="file">Choose a file</label>
+                    </div>
+                    <div className="NewPost-description">
+                        <textarea name="description" type="text" value={this.state.description} onChange={this.onTextChange} placeholder="Add description..."/>
+                    </div>
+                </div>
                 
-                <div className="NewPost-user-image">
-                    <img src={this.state.imageUrl} alt="Saturn"/>
-                </div>
-                <div className="NewPost-description">
-                    <textarea name="description" type="text" value={this.state.description} onChange={this.onTextChange} placeholder="Add description..."/>
-                </div>
                 
             </div>
         );
